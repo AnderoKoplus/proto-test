8c53f84554e80061ecba0eb3ba7172d1bd0f0ab5
Andero Koplus <andero.koplus@gmail.com>
2016-12-13 08:02:13 +0200

    initial setup

---
+++ build-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    clientName: 'foo',
+    projectName: 'bar',
+    buildDirectory: 'public-frontend/www',
+    npmBuildCommand: 'build',
+    branchesToFollow: ['master', 'development']
+};
